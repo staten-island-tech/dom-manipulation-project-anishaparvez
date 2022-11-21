346607a7acb347bd0ff39c5966b46be959659e12
anishaparvez <113203774+anishaparvez@users.noreply.github.com>
2022-11-21 14:59:26 -0500

    save

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
+  submitForm: document.querySelector("form"),
   text: document.querySelector("#text"),
-  box: document.getElementById("big-black-box"),
-  points: document.querySelectorAll(".point"),
+  box: document.getElementById("song"),
+  Album: document.getElementById("title"),
+  Singer: document.getElementById("singer"),
+  GetRid: document.getElementById("Remove"),
+  input: document.querySelector("text"),
+  displaySection: document.getElementById("display"),
 };
-
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.innerHTML = "This is now a big red box";
-  text.style.fontsize = "40px";
+ 
+function clearfields() {
+  DOMSelectors.Album.value = "Mind Of Mine";
+  DOMSelectors.Singer.value = "Zayn";
+  DOMSelectors.url.value =
+    "https://www.google.com/search?q=mind+of+mine&rlz=1C1GCEU_enUS1022US1022&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjhgK2h67X7AhUgEVkFHeqcDzcQ_AUoAnoECAoQBA&biw=1920&bih=969&dpr=1#imgrc=20e0e99YCba40M";
 }
-
-backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+DOMSelectors.submitForm.addEventListener("submit", function (event) {
+  let Singer = DOMSelectors.Singer.value;
+  let Artist = DOMSelectors.Artist.value;
+  let URL = DOMSelectors.URL.value;
+ 
+  event.preventDefault();
+ 
+  DOMSelectors.displaySection.insertAdjacentHTML(
+    "beforeend",
+    <div class="display-card">
+      <h2 class="display-singer">{singer}</h2>
+      <h3 class="display-album">{Album}</h3>
+      <img class="display-img" src="${URL}" />
+      <button class="remove-btn">Remove Album</button>
+    </div>
+  );
+  clearfields();
 });
